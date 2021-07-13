81e80217186bd5a6fa5fd823a173366cad479e94
E-Srgnk <e.srgnk@gmail.com>
2021-07-13 18:02:25 +0300

    added constants

---
+++ app/src/main/java/com/srgnk/news/commons/Constants.kt
@@ -0,0 +1,9 @@
+package com.srgnk.news.commons
+
+const val BASE_URL = "https://newsapi.org/"
+
+const val ITEM_TITLE = "title"
+const val ITEM_URL_TO_IMAGE = "urlToImage"
+const val ITEM_URL = "url"
+const val ITEM_PUBLISHED_AT = "publishedAt"
+const val ITEM_CONTENT = "content"
